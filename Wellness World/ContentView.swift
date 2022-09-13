2a4fd567f4f96049cb824b520df96e445b33ad0a
therealzoyak <therealzoyak@gmail.com>
2022-09-12 22:17:12 -0700

    first commit

--- Wellness World/ContentView.swift
+++ Wellness World/ContentView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import Firebase
 
 struct ContentView: View {
     @State private var email = ""
--- Wellness World/Wellness_WorldApp.swift
+++ Wellness World/Wellness_WorldApp.swift
@@ -6,9 +6,14 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Wellness_WorldApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
     var body: some Scene {
         WindowGroup {
             ContentView()
